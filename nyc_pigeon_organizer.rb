6a6aaca1517e6cce4e23cb89d660210d4cc95bfe
Buford Eeds <bufordeeds8@gmail.com>
2019-10-12 16:20:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,12 @@
 def nyc_pigeon_organizer(data)
 newHash = {
-  :key1 => {}
-  :key2 => {}
-  :key3 => {}
-  :key4 => {}
-  :key5 => {}
-  :key6 => {}
+  :key1 => {},
+  :key2 => {},
+  :key3 => {},
+  :key4 => {},
+  :key5 => {},
+  :key6 => {},
   :key7 => {}
 }
 end
+
